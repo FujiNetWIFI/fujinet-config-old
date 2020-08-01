41a38147ee99cf6a5a545f1a87e28601bd364839
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2020-08-01 15:22:33 -0500

    implement connect_wifi state.

--- src/error.c
+++ src/error.c
@@ -12,14 +12,21 @@
 char text_error_reading_adapter_config[] = "ERROR READING ADAPTER CONFIG";
 char text_error_scanning_networks[] = "ERROR SCANNING NETWORKS";
 char text_error_reading_scan_result[] = "ERROR READING SCAN RESULT";
+char text_error_reading_net_config[] = "ERROR READING NET CONFIG";
 char text_error_setting_ssid[] = "ERROR SETTING SSID";
+char text_error_reading_wifi_status[] = "ERROR READING WIFI STATUS";
+char text_error_wifi_no_ssid_available[] = "NO SSID AVIALABLE";
+char text_error_wifi_connection_successful[] = "CONNECTED TO NETWORK";
+char text_error_wifi_connect_failed[] = "CONNECT FAILED";
+char text_error_wifi_connection_lost[] = "CONNECTION LOST";
+char text_error_wifi_disconnected[] = "WIFI DISCONNECTED";
 
 /**
- * Display a fatal error, then die.
+ * Display error.
  * 
  * msg = An error message from the enum
  */
-void error_fatal(ErrorMessage msg)
+void error(ErrorMessage msg)
 {
   char* text = NULL;
   
@@ -34,11 +41,41 @@ void error_fatal(ErrorMessage msg)
     case ERROR_READING_SCAN_RESULT:
       text = text_error_reading_scan_result;
       break;
+    case ERROR_READING_NET_CONFIG:
+      text = text_error_reading_net_config;
+      break;
     case ERROR_SETTING_SSID:
       text = text_error_setting_ssid;
       break;
-    }
-  
+    case ERROR_READING_WIFI_STATUS:
+      text = text_error_reading_wifi_status;
+      break;
+    case ERROR_WIFI_NO_SSID_AVAILABLE:
+      text = text_error_wifi_no_ssid_available;
+      break;
+    case ERROR_WIFI_CONNECTION_SUCCESSFUL:
+      text = text_error_wifi_connection_successful;
+      break;
+    case ERROR_WIFI_CONNECT_FAILED:
+      text = text_error_wifi_connect_failed;
+      break;
+    case ERROR_WIFI_CONNECTION_LOST:
+      text = text_error_wifi_connection_lost;
+      break;
+    case ERROR_WIFI_DISCONNECTED:
+      text = text_error_wifi_disconnected;
+      break;
+    }  
   screen_puts(0,21,text);
+}
+
+/**
+ * Display a fatal error, then die.
+ * 
+ * msg = An error message from the enum
+ */
+void error_fatal(ErrorMessage msg)
+{
+  error(msg);
   die();
 }
--- src/error.h
+++ src/error.h
@@ -12,7 +12,14 @@ typedef enum _errorMessage
    ERROR_READING_ADAPTER_CONFIG,
    ERROR_SCANNING_NETWORKS,
    ERROR_READING_SCAN_RESULT,
-   ERROR_SETTING_SSID
+   ERROR_READING_NET_CONFIG,
+   ERROR_SETTING_SSID,
+   ERROR_READING_WIFI_STATUS,
+   ERROR_WIFI_NO_SSID_AVAILABLE,
+   ERROR_WIFI_CONNECTION_SUCCESSFUL,
+   ERROR_WIFI_CONNECT_FAILED,
+   ERROR_WIFI_CONNECTION_LOST,
+   ERROR_WIFI_DISCONNECTED
   } ErrorMessage;
 
 /**
@@ -21,5 +28,13 @@ typedef enum _errorMessage
  * msg = An error message from the enum
  */
 void error_fatal(ErrorMessage msg);
+
+/**
+ * Display error.
+ * 
+ * msg = An error message from the enum
+ */
+void error(ErrorMessage msg);
+
 
 #endif /* ERROR_H */
