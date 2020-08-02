ee94a3e6187405cefc8c7a82e8d83dee35f67d00
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2020-08-02 12:58:43 -0500

    add keys-reference functions

---
+++ src/keys-reference.h
@@ -0,0 +1,15 @@
+/**
+ * #FUJINET CONFIG
+ *
+ * Routines to display available keys for a mode
+ */
+
+#ifndef KEYS_REFERENCE_H
+#define KEYS_REFERENCE_H
+
+/**
+ * Keys used in Diskulator_hosts hosts mode
+ */
+void keys_reference_diskulator_hosts_hosts(void);
+
+#endif /* KEYS_REFERENCE_H */
