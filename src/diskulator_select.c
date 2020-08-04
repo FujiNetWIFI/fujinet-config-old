c6dc2d4f6cb4ac74d321611023e9eef971792c45
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2020-08-04 12:44:57 -0500

    add directory pagination conversion functions.

--- src/diskulator_select.c
+++ src/diskulator_select.c
@@ -12,6 +12,34 @@ typedef enum _substate
    DONE
   } SubState;
 
+typedef unsigned short Page;
+typedef unsigned char PageOffset;
+
+/**
+ * Convert directory position to page and offset
+ */
+void diskulator_select_pos_to_page(DirectoryPosition pos, Page* page, PageOffset* offset)
+{
+  *page=pos>>4;
+  *offset=pos&0x0F;
+}
+
+/**
+ * Convert page/offset to directory position
+ */
+void diskulator_select_page_to_pos(Page page, PageOffset offset, DirectoryPosition* pos)
+{
+  *pos=(page<<4)+offset;
+}
+
+/**
+ * Select file
+ */
+void diskulator_select_select_file(Context* context, SubState* ss)
+{
+  
+}
+  
 /**
  * Setup Diskulator Disk Images screen
  */
@@ -24,7 +52,7 @@ void diskulator_select_setup(Context *context)
 	      "PICK \xD9\xA5\xB3\xA3\x19"
 	      "ABORT");
   
-  screen_puts(0, 1, context->directory); 
+  screen_puts(0, 1, context->directory);
 }
 
 /**
@@ -36,6 +64,16 @@ State diskulator_select(Context *context)
   SubState ss=SELECT_FILE;
 
   diskulator_select_setup(context);
+
+  while (ss != DONE)
+    {
+      switch(ss)
+	{
+	case SELECT_FILE:
+	  diskulator_select_select_file(context,&ss);
+	  break;
+	}
+    }
   
   return new_state;
 }
