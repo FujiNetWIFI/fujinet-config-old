36158640c74d086ac791fc1f5bef4355b59b3fd0
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2020-08-12 20:16:53 -0500

    let's try that.

--- src/diskulator_select.c
+++ src/diskulator_select.c
@@ -427,7 +427,6 @@ State diskulator_select(Context *context)
 	case ADVANCE_DIR:
 	  context->dir_page=0;
 	  context->dir_eof=false;
-	  memset(context->filter,0,sizeof(context->filter));
 	  ss=SELECT_FILE;
 	  break;
 	case DEVANCE_DIR:
@@ -435,7 +434,6 @@ State diskulator_select(Context *context)
 	  diskulator_select_display_directory_path(context);
 	  context->dir_page=0;
 	  context->dir_eof=false;
-	  memset(context->filter,0,sizeof(context->filter));
 	  ss=SELECT_FILE;
 	  break;
 	}
